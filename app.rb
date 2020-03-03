cd6e9e77e5540a8a8a585704417f2115d6d7c2f9
Avril Harding <aviharding@gmail.com>
2020-03-03 16:24:30 +0000

    entering players

--- app.rb
+++ app.rb
@@ -1,10 +1,20 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  # get '/' do
+  #   "Hello Battle"
+  #   'Testing infrastructure working!'
+  # end
+
   get '/' do
-    "Hello Battle"
-    'Testing infrastructure working!'
+    erb :index
   end
 
-  run! if app_file == $0
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
+run! if app_file == $0
 end

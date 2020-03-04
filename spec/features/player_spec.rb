46213795849a77182cb42bab1b86e4050975558b
Avril Harding <aviharding@gmail.com>
2020-03-04 15:58:26 +0000

    attack player 2, extract logic the model, implementing hit points, sinlge responsibility principle

---
+++ spec/features/player_spec.rb
@@ -0,0 +1,25 @@
+require 'player'
+
+describe Player do
+  subject(:dave) { Player.new('Dave') }
+  subject(:mittens) {Player.new('Mittens') }
+
+  describe '#name' do
+    it 'returns the name' do
+      expect(dave.name).to eq 'Dave'
+    end
+  end
+
+  describe '#hit points' do
+    it 'returns the hit points' do
+      expect(mittens).to receive(:receive_damage)
+      dave.attack(mittens)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { dave.receive_damage }.to change { dave.hit_points}.by(-10)
+    end
+  end
+end
